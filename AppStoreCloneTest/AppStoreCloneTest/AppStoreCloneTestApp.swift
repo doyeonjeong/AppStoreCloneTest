8df8f013a59c83f79b096f111d71a178c07420af
doyeonjeong <debby_@kakao.com>
2023-10-06 03:08:33 +0900

    🎉[Init]: AppStoreCloneTest Project

---
+++ AppStoreCloneTest/AppStoreCloneTest/AppStoreCloneTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppStoreCloneTestApp.swift
+//  AppStoreCloneTest
+//
+//  Created by DOYEON JEONG on 2023/10/06.
+//
+
+import SwiftUI
+
+@main
+struct AppStoreCloneTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppStoreCloneTest/AppStoreCloneTestUITests/AppStoreCloneTestUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  AppStoreCloneTestUITestsLaunchTests.swift
+//  AppStoreCloneTestUITests
+//
+//  Created by DOYEON JEONG on 2023/10/06.
+//
+
+import XCTest
+
+final class AppStoreCloneTestUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
